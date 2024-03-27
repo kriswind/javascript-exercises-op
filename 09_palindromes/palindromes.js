c8d9cd383739eb40f75a1d22f125c5a4da251441
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-27 17:37:01 +0200

    created palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (old_word = "") {
+  old_word = old_word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  var new_word = "";
+  for (var i = 0; i < old_word.length; i++) {
+    new_word += old_word[old_word.length - i - 1];
+  }
 
+
+  if (new_word == old_word) return true;
+  return false;
 };
 
 // Do not edit below this line
