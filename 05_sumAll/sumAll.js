3f03af59fc3283e92f8e26bc6495c2f730442b87
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-27 14:46:22 +0200

    sum all completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  if (x < 0 || y < 0) return "ERROR"
+  if ((typeof x !== 'number') || (typeof y !== 'number')) return "ERROR"
+  if (x > y) {
+    let tmp = x
+    x = y
+    y = tmp
+  }
+  var sum = 0;
+  for (var i = x; i < y + 1; i++) {
+    sum += i;
+  };
+  return sum
 };
 
 // Do not edit below this line
