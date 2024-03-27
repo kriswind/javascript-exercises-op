0e2547b1a9c3c11ad69d5233ff991f7ebeb0859b
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-27 17:46:30 +0200

    scuffed fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n);
+  if (n < 0) return "OOPS";
+  if (n <= 1) return n;
 
+  let prev = 0;
+  let curr = 1;
+  let next;
+
+  for (let i = 2; i <= n; i++) {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
