0585b6e264ccb3aa0f21bb12cdbb01e7891c556b
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-27 17:21:13 +0200

    created calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+  let total = 0;
+  args.forEach(function(arg) {
+    total += parseInt(arg);
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function([...args]) {
+  let total = 0;
+  args.forEach(function(arg) {
+    if (total == 0) {total = arg; return;}
+    total = total * arg;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  //base case for 0!
+  if (n == 0 || n == 1) {
+    return 1;
+    //recursion case
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
