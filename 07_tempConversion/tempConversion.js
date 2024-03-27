56b50259956ef4b38c3f61f06f82e96b5ea44183
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-27 15:40:19 +0200

    temp conversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  var cel_num = (fahr - 32) * (5/9)
+  var cels = Math.round(cel_num*10)/10
+  return cels
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  var fahr_num = (9/5 * cels) + 32
+  var fahr = Math.round(fahr_num*10)/10
+  return fahr
 };
 
 // Do not edit below this line
